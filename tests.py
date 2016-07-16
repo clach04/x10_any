0b19d641f51e0562c18f55009f805b1ad9acc142
clach04 <Chris.Clark@actian.com>
2016-07-16 10:12:19 -0700

    Start of tests

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: us-ascii -*-
 # vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
 #
@@ -7,9 +7,49 @@ import os
 import sys
 from unittest import main, TestCase
 
+# import x10_any
+
 
 class TestUtils(TestCase):
-    pass
+    def test_validate_house_code_a_upper(self):
+        test_value = 'A'
+        canon = 'A'
+        result = x10_any.normalize_housecode(test_value)
+        self.assertEqual(canon, result)
+
+    def test_validate_house_code_a_lower(self):
+        test_value = 'a'
+        canon = 'A'
+        result = x10_any.normalize_housecode(test_value)
+        self.assertEqual(canon, result)
+
+    def test_validate_house_code_p_upper(self):
+        test_value = 'P'
+        canon = 'A'
+        result = x10_any.normalize_housecode(test_value)
+        self.assertEqual(canon, result)
+
+    def test_validate_house_code_p_lower(self):
+        test_value = 'p'
+        canon = 'A'
+        result = x10_any.normalize_housecode(test_value)
+        self.assertEqual(canon, result)
+
+    def test_validate_house_code_q_upper(self):
+        test_value = 'Q'
+        canon = 'A'
+
+        def doit():
+            x10_any.normalize_housecode(test_value)
+        self.failUnlessRaises(ValueError, doit)  # not sure I see the need for a x10_any.??Exception
+
+    def test_validate_house_code_q_lower(self):
+        test_value = 'q'
+        canon = 'A'
+
+        def doit():
+            x10_any.normalize_housecode(test_value)
+        self.failUnlessRaises(ValueError, doit)  # not sure I see the need for a x10_any.??Exception
 
 
 if __name__ == "__main__":
