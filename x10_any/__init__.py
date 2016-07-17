149723ded3a7883e5c891ecca1e83f4e066ab12c
clach04 <Chris.Clark@actian.com>
2016-07-17 10:27:52 -0700

    cm17a support should be optional

--- x10_any/__init__.py
+++ x10_any/__init__.py
@@ -26,7 +26,10 @@ except ImportError:
         # WARNING all on/off not supported with this module :-(
     except:
         firecracker = None
-        import x10_any.cm17a as x10  # Use internal Python 3 compatible copy of http://www.averdevelopment.com/python/x10.html
+        try:
+            import x10_any.cm17a as x10  # Use internal Python 3 compatible copy of http://www.averdevelopment.com/python/x10.html
+        except ImportError:
+            x10 = None
 
 
 try:
