8da70e74a812f89ddc373bf826827ac33c4e6e5d
clach04 <clach04@gmail.com>
2023-08-19 15:33:14 -0700

    pip supported dev version

--- x10_any/_version.py
+++ x10_any/_version.py
@@ -1,2 +1,2 @@
-version_tuple = __version_info__ = (0, 0, 9, 'git')
+version_tuple = __version_info__ = (0, 0, 9, 'dev3')
 version = version_string = __version__ = '.'.join(map(str, __version_info__))
